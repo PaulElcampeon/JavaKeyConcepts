24df7cfbf2c495ecacaea2abe986cfa929c00a09
Paul Oladele <pauloladele@live.co.uk>
2018-02-04 19:11:19 +0000

    What is an instance?

---
+++ JavaKeyConcepts/Instance/instance/Main.java
@@ -0,0 +1,13 @@
+package instance;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		/*freddy is an instance of the Penguin class
+		 * or you can say freddy is an object of the 
+		 *Penguin class*/
+		Penguin freddy = new Penguin();
+		
+	}
+
+}
